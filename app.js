382e3cf2e017c1149748b347b2eaec5ed2bba2fa
Julianb12 <julianb1212@gmail.com>
2019-08-19 15:22:35 -0500

    Caesar Cypher

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Sum Drill
 const express = require('express');
 const morgan = require('morgan');
 
@@ -28,4 +29,34 @@ app.get('/sum', (req, res) => {
 
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000');
+});
+/////////////////////////
+// Caesar Cipher
+app.get('/caesar', (req, res) => {
+  const text = req.query.text;
+  const shift = req.query.shift;
+
+  if (!text) {
+    return res.status(400).send('Please provide text');
+  }
+
+  if (!shift) {
+    return res.status(400).send('Please provide a number');
+  }
+
+  const shiftNum = parseFloat(shift);
+
+  const base = 'A'.charCodeAt(0);
+
+  const cipher = text.toUpperCase().split('').map(char => {
+    const code = char.charCodeAt(0);
+    let difference = code - base;
+    difference = difference + shiftNum;
+    difference = difference % 26;
+    const shiftedLetter = String.fromCharCode(base + difference);
+    return shiftedLetter;
+  })
+  .join('');
+
+  res.status(200).send(cipher);
 });
